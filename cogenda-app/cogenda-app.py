47abc21207c6c67171118bb956461aef0c573160
tim.tang <tang.jilong@139.com>
2014-06-08 12:13:03 +0800

    [cogenda-web] - Refactor code.

--- cogenda-app/cogenda-app.py
+++ cogenda-app/cogenda-app.py
@@ -18,20 +18,25 @@ def main(settings_file):
 
     root_dir = abspath(dirname(init_files[0]))
     server = Server(root_dir=root_dir)
+    dispatcher = register_routes(server)
+    try:
+        server.start(settings_file, dispatcher)
+    except KeyboardInterrupt:
+        server.stop()
 
+
+def register_routes(server):
     """ Register router dispatchers """
     dispatcher = cherrypy.dispatch.RoutesDispatcher()
     for controller_type in BaseController.all():
         controller = controller_type(server=server)
         controller.register_routes(dispatcher)
+    return dispatcher
 
-    try:
-        server.start(settings_file, dispatcher)
-    except KeyboardInterrupt:
-        server.stop()
     
 def usage():
     print("usage: python cogenda-app/cogendap-app.py <settings_file>")
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
