31216edb183512da86c52b5d3e2b8e7e19d798bd
Johnny.Lv <kkiiiu@gmail.com>
2014-06-10 23:02:39 +0800

    [cogenda-web] - For Admin module

--- cogenda-app/cogenda-app.py
+++ cogenda-app/cogenda-app.py
@@ -5,6 +5,8 @@ from lib.fs import *
 import sys
 from os.path import join, dirname, abspath, exists, split
 import cherrypy
+import hashlib
+import os
 from lib.controller import BaseController
 
 from controllers import *
@@ -19,6 +21,8 @@ def main(settings_file):
     root_dir = abspath(dirname(init_files[0]))
     server = Server(root_dir=root_dir)
     dispatcher = register_routes(server)
+    register_jinja(server)
+
     try:
         server.start(settings_file, dispatcher)
     except KeyboardInterrupt:
@@ -33,6 +37,33 @@ def register_routes(server):
         controller.register_routes(dispatcher)
     return dispatcher
 
+def register_jinja(server):
+
+    '''
+    if not hasattr(server, '_static_hash'):
+        server._static_hash = {}
+
+    def static_url(filename):
+        if server.testing:
+            return filename
+
+        if filename in server._static_hash:
+            return server._static_hash[filename]
+
+        with open(os.path.join(server.static_folder, filename), 'r') as f:
+            content = f.read()
+            hsh = hashlib.md5(content).hexdigest()
+
+        server.logger.info('Generate %s md5sum: %s' % (filename, hsh))
+        prefix = server.config.get('SITE_STATIC_PREFIX', '/static/')
+        value = '%s%s?v=%s' % (prefix, filename, hsh[:5])
+        server._static_hash[filename] = value
+        return value
+
+    @server.context_processor
+    def register_context():
+        return dict(static_url=static_url,)
+    '''
     
 def usage():
     print("usage: python cogenda-app/cogendap-app.py <settings_file>")
