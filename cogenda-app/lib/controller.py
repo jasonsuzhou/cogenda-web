9efdd56f731a96f7d218f84ba0a40ca0f5c9a63a
tim.tang <tang.jilong@139.com>
2014-06-11 20:56:34 +0800

    [cogenda-web] - Fix Jinja2 cannot translate in daemon mode.

--- cogenda-app/lib/controller.py
+++ cogenda-app/lib/controller.py
@@ -133,10 +133,11 @@ class BaseController(object):
     def render_template(self, template_file, **kw):
         """ Integrate with Jinja2 & Babel"""
         app_name = self.settings.cogenda_app.app_name
-        mo_dir = os.path.join(os.path.abspath(os.curdir),app_name ,'i18n')
-        print mo_dir
+        mo_dir = os.path.join(os.path.abspath(os.curdir), app_name ,'i18n')
+        if self.settings.cogenda_app.as_bool('daemon'):
+            mo_dir = self.settings.cogenda_app.app_home + mo_dir
+        log.debug(mo_dir)
         locale = str(cherrypy.response.i18n.locale)
-        print locale
         translations = Translations.load(mo_dir, locale, app_name)
         env = Environment(loader = PackageLoader(app_name, 'templates'), extensions=['jinja2.ext.i18n'])
         env.install_gettext_translations(translations)
