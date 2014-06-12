4307743748de3cda7bf15a0c7ef9064882ebb047
Johnny.Lv <kkiiiu@gmail.com>
2014-06-12 17:52:23 +0800

    [cogenda-web][skip-ci] - For user mgmt

--- cogenda-app/controllers/admin/admin_controller.py
+++ cogenda-app/controllers/admin/admin_controller.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from models import User
 import cherrypy
 from lib.i18ntool import ugettext as _
+from json import dumps
 
 class AdminController(BaseController):
 
@@ -24,29 +25,31 @@ class AdminController(BaseController):
         return self.render_template('admin/security/login-user.html', date=datetime.now(), hello=_('hello'))
 
     @route('/admin/user-mgmt')
-    def userMgmt(self):
-
-        #cherrypy.tools.I18nTool.set_custom_language('en_US') 'zh_CN'
-
-        """ testing SQLite and Jinja2 """
-        #user = User('Tim', '123', 'tang.jilong@gmail.com')
-        #cherrypy.request.db.add(user)
-        #all_users = User.list(cherrypy.request.db)
-        #for temp_user in all_users:
-        #    print temp_user.username
-
+    def user_mgmt(self):
         return self.render_template('admin/user-mgmt/user-container.html', date=datetime.now(), hello=_('hello'))
 
     @route('/admin/resource-mgmt')
-    def resourceMgmt(self):
-
-        #cherrypy.tools.I18nTool.set_custom_language('en_US') 'zh_CN'
-
-        """ testing SQLite and Jinja2 """
-        #user = User('Tim', '123', 'tang.jilong@gmail.com')
-        #cherrypy.request.db.add(user)
-        #all_users = User.list(cherrypy.request.db)
-        #for temp_user in all_users:
-        #    print temp_user.username
-
+    def resource_mgmt(self):
         return self.render_template('admin/resource-mgmt/resource-container.html', date=datetime.now(), hello=_('hello'))
+
+    @route('/admin/user-mgmt-data')
+    @cherrypy.tools.json_out()
+    def user_mgmt_data(self):
+        all_users = User.list(cherrypy.request.db)
+        users_in_json = []
+        #print all_users
+        for user in all_users:
+            users_in_json.append(self.to_json(user))
+        return users_in_json
+
+    def to_json(self, model):
+        """ Returns a JSON representation of an SQLAlchemy-backed object.
+        """
+        json = {}
+        json['fields'] = {}
+        json['pk'] = getattr(model, 'id')
+
+        for col in model._sa_class_manager.mapper.mapped_table.columns:
+            json['fields'][col.name] = getattr(model, col.name)
+
+        return dumps([json])
---
+++ cogenda-app/static/js/admin/cogenda.admin.js
@@ -0,0 +1,264 @@
+$(document).ready(function() {
+
+    // Establish history variable
+    var History = window.History;
+    var State = History.getState();
+
+    // Bind state change.
+    History.Adapter.bind(window, 'statechange', function() {
+        var State = History.getState();
+        $.ajax({
+            url: State.url,
+            success: function(msg) {
+                $('#main-content').html($(msg).find('#main-content').html());
+                $('#main-content').fadeIn();
+                var newTitle = $(msg).filter('title').text();
+                $('title').text(newTitle);
+                ready_optimized_page(window.location.pathname);
+                //$('#loading').remove();
+            }
+        });
+    });
+
+    // Handle menu click event.
+    $('ul.cl-vnavigation li').each(function(index, li) {
+        $(li).click(function(e) {
+            var sub_menus = $(li).find('ul');
+            if (sub_menus.length > 0) {
+                return;
+            }
+            e.preventDefault();
+            var parent = $('#main-content');
+            var loading = $('<div id="loading" class="loading"><i class="fa fa-spinner"></i></div>');
+            loading.appendTo(parent);
+            loading.fadeIn(0);
+            var $clink = li.children[0];
+            History.pushState({
+                state: 1,
+                rand: Math.random()
+            }, null, $clink.href);
+            $('ul.cl-vnavigation li.active').removeClass('active');
+            $(li).addClass('active');
+        });
+    });
+    ready_optimized_page(window.location.pathname);
+});
+
+/**
+ *
+ * Document ready for optimized pages.
+ * TODO: Add details pages & dynamic menu handling.
+ */
+function ready_optimized_page(uri) {
+
+    ready_navigation_menu();
+
+    switch (uri) {
+    case "/admin/user-mgmt":
+        ready_user_mgmt();
+        break;
+    case "/admin/resource-mgmt":
+        ready_resource_mgmt();
+        break;
+    }
+}
+
+/**
+ * Document ready for navigation menu.
+ */
+function ready_navigation_menu() {
+    // Handle menu click event.
+    $('ul.cl-vnavigation li').each(function(index, li) {
+        var sub_menus = $(li).find('ul');
+        //if (sub_menus.length > 0) {
+        //    return;
+        //}
+        var $clink = li.children[0];
+        if ($clink.href == String(window.location)) {
+            $(this).addClass('active');
+        }
+    });
+}
+
+/**
+ * Document ready js for user management page.
+ */
+function ready_user_mgmt() {
+    // Ready common datatable.
+    ready_common_datatable("user-mgmt-datatable", "/admin/user-mgmt-data", function(datatable) {
+        // Add new user in datatable.
+        $("#user-mgmt-proceed").click(function(e) {
+            datatable.fnAddData(["Trident-new", "Internet Explorer 4.0", "Win 95+", "4", "X"]);
+        });
+
+        // Delete user in datatable.
+        $("#user-mgmt-delete").click(function(e) {
+            e.preventDefault();
+            fnRemoveSelected(datatable);
+        });
+    });
+}
+
+/**
+ * Document ready js for resource management page.
+ * TODO: Integrate with new datatable fw.
+ */
+function ready_resource_mgmt() {
+    // Ready common datatable.
+    ready_common_datatable('resource-mgmt-datatable', "/admin/resource-mgmt-data", function(datatable) {
+
+        // Add new resource in datatable.
+        $("#resource-mgmt-proceed").click(function(e) {
+            datatable.fnAddData(["Trident-new", "Internet Explorer 4.0", "Win 95+", "4", "X"]);
+        });
+
+        // Delete resource in datatable.
+        $("#resource-mgmt-delete").click(function(e) {
+            e.preventDefault();
+            fnRemoveSelected(datatable);
+        });
+    });
+}
+
+//***************************************************************
+//
+//              COMMON UI COMPONENTS
+//
+//***************************************************************
+/**
+ * Common ready for select2.
+ *
+ */
+function ready_common_select2() {
+    /*Select2*/
+    $(".select2").select2({
+        width: '100%'
+    });
+}
+
+/**
+ * Common ready for switch.
+ *
+ */
+function ready_common_switch() {
+    /*Switch*/
+    $('.switch').bootstrapSwitch();
+}
+
+/**
+ * Common ready for switch.
+ *
+ */
+function ready_common_multi_select_group() {
+    /*Multi-Select Group*/
+    $('.privilegeMultiSelect').multiSelect({
+        selectableOptgroup: true
+    });
+}
+
+/**
+ * Common ready for searchable multi select.
+ *
+ */
+function ready_common_searchable_multi_select() {
+    /*Multi-Select Search*/
+    $('.searchable').multiSelect({
+        selectableHeader: "<input type='text' class='form-control search-input' autocomplete='off' placeholder='Filter String'>",
+        selectionHeader: "<input type='text' class='form-control search-input' autocomplete='off' placeholder='Filter String'>",
+        afterInit: function(ms) {
+            var that = this,
+                $selectableSearch = that.$selectableUl.prev(),
+                $selectionSearch = that.$selectionUl.prev(),
+                selectableSearchString = '#' + that.$container.attr('id') + ' .ms-elem-selectable:not(.ms-selected)',
+                selectionSearchString = '#' + that.$container.attr('id') + ' .ms-elem-selection.ms-selected';
+
+            that.qs1 = $selectableSearch.quicksearch(selectableSearchString).on('keydown', function(e) {
+                if (e.which === 40) {
+                    that.$selectableUl.focus();
+                    return false;
+                }
+            });
+
+            that.qs2 = $selectionSearch.quicksearch(selectionSearchString).on('keydown', function(e) {
+                if (e.which == 40) {
+                    that.$selectionUl.focus();
+                    return false;
+                }
+            });
+        },
+        afterSelect: function() {
+            this.qs1.cache();
+            this.qs2.cache();
+        },
+        afterDeselect: function() {
+            this.qs1.cache();
+            this.qs2.cache();
+        }
+    });
+}
+
+/**
+ * Common ready for tables.
+ *
+ * @param datatable identifier
+ */
+function ready_common_datatable(datatable_id, url, fnDatatableCallback) {
+    $.ajax({
+        "dataType": 'json',
+        "type": "GET",
+        "url": url,
+        "success": function(result) {
+            console.log(result);
+            $('.table-responsive').html('<table class="table table-bordered" id="' + datatable_id + '"></table>');
+            var datatable_id = "#" + datatable_id;
+
+            /* Init the table with dynamic ajax loader.*/
+            var datatable = $(datatable_id).dataTable({
+                "aaData": result.aaData,
+                "aoColumns": result.aoColumns
+            });
+
+            // Add/remove class to a row when clicked on
+            $(datatable_id).on('click', 'tbody tr', function(e) {
+                $(this).toggleClass('row_selected');
+            });
+
+            // Search input style
+            $('.dataTables_filter input').addClass('form-control').attr('placeholder', 'Search');
+            $('.dataTables_length select').addClass('form-control');
+
+            // Callback method for datatable
+            fnDatatableCallback(datatable);
+        }
+    });
+}
+
+function authenticate() {
+    if ($('#security-login-form').parsley().validate()) {
+        var username = $('#username').val();
+        var password = $('#password').val();
+        eventBus.trigger('security:show-loading');
+        authenticationProvider.authenticate({
+            username: username,
+            password: password
+        });
+    } else {
+        console.log('Client side validate error.');
+    }
+}
+
+//***************************************************************
+//
+//              COMMON JAVASCRIPT METHODS
+//
+//***************************************************************
+/*
+ * I don't actually use this here, but it is provided as it might be useful and demonstrates
+ * getting the TR nodes from DataTables
+ */
+function fnRemoveSelected(local_table) {
+    var selected_rows = local_table.$('tr.row_selected');
+    selected_rows.each(function(index, row) {
+        local_table.fnDeleteRow(row);
+    });
+}
