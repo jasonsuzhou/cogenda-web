d0988bec24c5081a62944259279172058d0e616f
Johnny.Lv <kkiiiu@gmail.com>
2014-06-21 23:30:06 +0800

    [cogenda-web] - Backend security login

--- cogenda-app/static/js/cogenda.admin.auth.js
+++ cogenda-app/static/js/cogenda.admin.auth.js
@@ -1,37 +1,35 @@
 
 $(document).ready(function() {
-    $('#login').on('click', function() {
-        authenticate();
+    $('#login').on('click', function(event) {
+        if (event) event.preventDefault();
+        if ($('#security-login-form').parsley().validate()) {
+            var username = $('#username').val().trim();
+            var password = $('#password').val().trim();
+
+            credentials = {
+                username: username,
+                password: password
+            };
+
+            var authenticate = $.ajax({
+                dataType: 'json',
+                contentType: "application/json",
+                url: '/admin/authenticate',
+                data: JSON.stringify(credentials),
+                type: 'POST'
+            });
+
+            authenticate.done(function(resp) {
+                console.log("Auth done!");
+                console.log(resp.auth_token);
+                window.location = '/admin/user-mgmt';
+            });
+
+            authenticate.fail(function(resp, status) {
+                //TODO: handle failure.
+            });
+        } else {
+            console.log('Client side validate error.');
+        }
     });
 });
-
-function authenticate() {
-    if ($('#security-login-form').parsley().validate()) {
-        var username = $('#username').val().trim();
-        var password = $('#password').val().trim();
-
-        credentials = {
-            username: username,
-            password: password
-        };
-
-        $.ajax({
-            dataType: 'json',
-            contentType: "application/json",
-            url: '/admin/authenticate',
-            data: JSON.stringify(credentials),
-            type: 'POST',
-            success: function(result) {
-                console.log("MMMMMMMMMMMMMMMMMMMM");
-            }
-        });
-
-        /*
-        authenticate.promise().done(function(resp) {
-            console.log("Auth done!");
-            self.location.href = '/admin/user-mgmt';
-        });*/
-    } else {
-        console.log('Client side validate error.');
-    }
-}
