72cc15dc1399d103456aa33db1233de78f373d48
Johnny.Lv <kkiiiu@gmail.com>
2014-06-25 23:02:39 +0800

    [cogenda-web] - Frondend login palette

---
+++ cogenda-app/static/js/cogenda.web.js
@@ -0,0 +1,47 @@
+
+$(document).ready(function() {
+
+    $('#login').on('click', function(event) {
+        if (event) event.preventDefault();
+        console.log(">>>>>>>>>>>>>Login palette show.");
+        $('#user-login-modal').show();
+        /*
+        if ($('#security-login-form').parsley().validate()) {
+            var username = $('#username').val().trim();
+            var password = $('#password').val().trim();
+
+            credentials = {
+                username: username,
+                password: password
+            };
+
+            var authenticate = $.ajax({
+                dataType: 'json',
+                contentType: "application/json",
+                url: '/security/authenticate',
+                data: JSON.stringify(credentials),
+                type: 'POST'
+            });
+
+            authenticate.done(function(resp) {
+                $('#password-error ul').children().remove();
+                console.log("Auth done!");
+                var result = JSON.parse(resp);
+                if (!result.auth_success) {
+                    // console.log(result.msg);
+                    $('#login-msg').text(result.msg);
+                    $('#login-msg-container').show();
+
+                    return;
+                }
+                window.location = result.refer;
+            });
+
+            authenticate.fail(function(resp, status) {
+                //TODO: display error msg on ui.
+            });
+        } else {
+            console.log('Client side validate error.');
+        }*/
+    });
+});
