4bcfd72c23e5c2877dca12a61309c7bca6a5722d
tim.tang <tang.jilong@139.com>
2014-06-30 21:32:07 +0800

    [cogenda-web] [skip ci] - Fix user can login backend bug.

--- cogenda-app/lib/controller.py
+++ cogenda-app/lib/controller.py
@@ -48,7 +48,7 @@ def authenticated(func):
     def actual(*arguments, **kw):
         instance = arguments[0]
         user = instance.user
-        if user:
+        if user[0] and user[1] == '3':
             return func(*arguments, **kw)
         else:
             raise cherrypy.HTTPRedirect('/admin/login')
